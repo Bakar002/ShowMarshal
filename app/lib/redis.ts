1b925f22565118829475bec003fe96e4c58e0a3a
Bakar002 <abubakaramjad78600@gmail.com>
2025-01-08 16:44:15 +0500

    adding

---
+++ app/lib/redis.ts
@@ -0,0 +1,6 @@
+import { Redis } from "@upstash/redis";
+
+export const redis = new Redis({
+  url:'https://humorous-sheepdog-39950.upstash.io',
+  token:'AZwOAAIjcDE3NWU4M2Y2ZjY2YzI0YjczYmMwYTA5YjJhMTFiOTYyN3AxMA',
+});
---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "utfs.io",
+        port: "",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
